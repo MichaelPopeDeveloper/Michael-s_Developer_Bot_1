59d15e9b6899e16e64cf05a1b63fa7832237897b
Michael Pope <popem3@ad.unlv.edu>
2019-04-29 20:54:30 -0700

    still cant send emial

--- index.py
+++ index.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+from controllers.sendMail import CreateMessage
+
+def main():
+    CreateMessage('michaelpopedeveloper.bot.1@gmail.com', 'michaelpopedeveloper@gmail.com', 'send email', 'Hello World!')
+
+main()
